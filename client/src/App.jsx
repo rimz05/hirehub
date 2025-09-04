3715e34d3959ac31ff36e5bf29cfe0c1dbdc624d
rimz05 <139325866+rimz05@users.noreply.github.com>
2025-09-04 19:20:24 +0530

    working 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import ApplyJobs from './pages/ApplyJobs'
+import Application from './pages/Application'
+import Dashboard from './pages/Dashboard'
+import ManagerJob from './pages/ManagerJob'
+import AddJobs from './pages/AddJobs'
+import ViewApplication from './pages/ViewApplication'
+import { AppContext } from './context/AppContext'
+import Recruiter from './components/Recruiter'
+import { ToastContainer } from 'react-toastify'
 
 const App = () => {
+
+  const {showRecruiterLogin, companyToken} = useContext(AppContext)
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-    </h1>
+    <div >
+      { showRecruiterLogin && <Recruiter/>}
+      <ToastContainer/>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/apply-job/:id' element={<ApplyJobs/>}/>
+      <Route path='/applications' element={<Application/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}>
+
+      {
+        companyToken?<>
+         <Route path='add-job' element={<AddJobs/>}/>
+        <Route path='manage-jobs' element={<ManagerJob/>}/>
+        <Route path='view-applications' element={<ViewApplication/>}/>
+        </>:null
+      }
+       
+      </Route>
+      </Routes>
+    </div>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from 'react-router-dom'
+import { AppContextProvider } from './context/AppContext.jsx'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <StrictMode>
+      <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+      </BrowserRouter>
   </StrictMode>,
+  </ClerkProvider>
 )
